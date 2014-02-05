c0ef3578afdb5d500249a5739f9702f2d3981e6f
Jan-Piet Mens <jpmens@gmail.com>
2014-02-05 11:50:06 +0000

    remove retain from example pub.sh

--- overlay/etc/mosquitto/pub.sh
+++ overlay/etc/mosquitto/pub.sh
@@ -7,5 +7,5 @@ tst=`date +%s`
 
 payload='{"tst":"'${tst}'","lat":"48.858334","_type":"location","lon":"2.295134","acc":"141", "batt":"27"}'
 
-mosquitto_pub -r -t ${topic} -m "${payload}"
+mosquitto_pub -t ${topic} -m "${payload}"
 
